2372708efa3c738f01211ebd0ceb587e3e597f90
Robert Horvat <robert.horvath93@gmail.com>
2023-11-18 19:14:20 +0200

    adding links

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='home'),  # '' means base website
-
+    path('item/<int:pk>', views.MenuItemDetail.as_view(), name='menu_item'),
 ]
 
